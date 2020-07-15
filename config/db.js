14930e894497097225a2e6ba6e91490febfeb322
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-07-16 00:58:07 +0430

    database connection file

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const { Client } = require("pg");
+
+const client = new Client({
+  user: process.env.DATABASE_USER,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
+});
+client
+  .connect()
+  .then(() => {
+    console.log(`Connected to database Successfully ... !`.cyan.underline.bold);
+  })
+  .catch(() => {
+    console.log(
+      `We have some problem to conncet to database ... !`.red.underline.bold
+    );
+  });
+
+module.exports = client;
