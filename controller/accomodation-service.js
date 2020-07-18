2b71842e64c9ebeb5d52306953deb439fe009331
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-07-18 20:12:20 +0430

    create accomodation API

--- controller/accomodation-service.js
+++ controller/accomodation-service.js
@@ -1,4 +1,6 @@
 const asyncHandler = require("../middlewares/async");
+const fs = require("fs");
+const path = require("path");
 const client = require("../config/db");
 
 // @desc        Get all accomodation
@@ -6,9 +8,20 @@ const client = require("../config/db");
 // @access      Public
 exports.getAccomodations = asyncHandler(async (req, res, next) => {
   client.query("select * from tbl_accomodationservice", (err, result) => {
-    res
-      .status(201)
-      .json({ success: true, count: result.rowCount, data: result.rows });
+    if (!err) {
+      res
+        .status(200)
+        .json({ success: true, count: result.rowCount, data: result.rows });
+      fs.appendFileSync(
+        `${path.dirname(require.main.filename)}/log/database-log.txt`,
+        "Select all Accomodations ... !\n"
+      );
+    } else {
+      fs.appendFileSync(
+        `${path.dirname(require.main.filename)}/log/database-log.txt`,
+        "Error while selecting all Accomodations ... !\n"
+      );
+    }
   });
 });
 
@@ -20,7 +33,18 @@ exports.getAccomodation = asyncHandler(async (req, res, next) => {
     "select * from tbl_accomodationservice where id = $1",
     [req.params.id],
     (err, result) => {
-      res.status(201).json({ success: true, data: result.rows });
+      if (!err) {
+        res.status(200).json({ success: true, data: result.rows });
+        fs.appendFileSync(
+          `${path.dirname(require.main.filename)}/log/database-log.txt`,
+          "Select Single Accomodation ... !\n"
+        );
+      } else {
+        fs.appendFileSync(
+          `${path.dirname(require.main.filename)}/log/database-log.txt`,
+          "Error while selecting Single Accomodation ... !\n"
+        );
+      }
     }
   );
 });
@@ -28,6 +52,43 @@ exports.getAccomodation = asyncHandler(async (req, res, next) => {
 // @desc        create new accomodation
 // @route       POST /api/accomodation
 // @access      Private
+exports.createAccomodation = asyncHandler(async (req, res, next) => {
+  client.query(
+    "update tbl_accomodationservice set active = false where active = true",
+    (err, result) => {
+      if (!err) {
+        fs.appendFileSync(
+          `${path.dirname(require.main.filename)}/log/database-log.txt`,
+          "update accomodations ... !\n"
+        );
+        const { explanation, learnmore, otherservice, guides } = req.body;
+        client.query(
+          "insert into tbl_accomodationservice (explanation,learnmore,otherservice,guides,active) values ($1 , $2 , $3, $4 , $5)",
+          [explanation, learnmore, otherservice, guides, true],
+          (err, result) => {
+            if (!err) {
+              res.status(201).json({ success: true, data: {} });
+              fs.appendFileSync(
+                `${path.dirname(require.main.filename)}/log/database-log.txt`,
+                "Create Single Accomodation ... !\n"
+              );
+            } else {
+              fs.appendFileSync(
+                `${path.dirname(require.main.filename)}/log/database-log.txt`,
+                "Error while Create Single Accomodation ... !\n"
+              );
+            }
+          }
+        );
+      } else {
+        fs.appendFileSync(
+          `${path.dirname(require.main.filename)}/log/database-log.txt`,
+          "Error while update Accomodations ... !\n"
+        );
+      }
+    }
+  );
+});
 
 // @desc        Update all accomodation
 // @route       PUT /api/accomodation
--- routes/accomodation-service.js
+++ routes/accomodation-service.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const {
   getAccomodations,
   getAccomodation,
+  createAccomodation,
 } = require("../controller/accomodation-service");
 
-router.route("/").get(getAccomodations);
+router.route("/").get(getAccomodations).post(createAccomodation);
 
 router.route("/:id").get(getAccomodation);
 
