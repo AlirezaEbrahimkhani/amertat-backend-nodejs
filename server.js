03378e5f20caf3cfad905a8c587a0cf75aca5da3
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-07-18 22:17:45 +0430

    fix access to back-end bug

--- server.js
+++ server.js
@@ -13,6 +13,31 @@ const accomodation = require("./routes/accomodation-service");
 // init application
 const app = express();
 
+// fix access to back-end
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 //body Parser
 app.use(express.json());
 
