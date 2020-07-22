111974a55119c1c218552f6522ef15dc6c1c145e
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-07-23 03:12:23 +0430

    contact us APi

--- routes/contact-us.js
+++ routes/contact-us.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const { createNewForm } = require("../controller/contact-us");
+
+router.route("/").post(createNewForm);
 
 module.exports = router;
