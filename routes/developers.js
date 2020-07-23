bfda112cfeaaf67728d54d73d531199924113724
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-07-23 18:22:43 +0430

    add developers API and routes

--- routes/developers.js
+++ routes/developers.js
@@ -1,4 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getDevelopers,
+  getDeveloper,
+  createDeveloper,
+  updateDeveloper,
+  deleteDeveloper,
+} = require("../controller/developers");
+
+router.route("/").get(getDevelopers).post(createDeveloper);
+
+router
+  .route("/:id")
+  .get(getDeveloper)
+  .put(updateDeveloper)
+  .delete(deleteDeveloper);
 
 module.exports = router;
