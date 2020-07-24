d2b8246cb639bf140bd5334636f1c6eb25722d70
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-07-25 00:58:18 +0430

    implement hash functionality

--- controller/auth.js
+++ controller/auth.js
@@ -8,9 +8,10 @@ const client = require("../config/db");
 // @access      Private
 exports.register = asyncHandler(async (req, res, next) => {
   const { first_name, last_name, user_name, password } = req.body;
+  const hashPass = await encryptPass(password);
   await client.query(
     "insert into tbl_users (first_name , last_name , user_name , password , role_id) values ($1 , $2 , $3 , $4 , $5)",
-    [first_name, last_name, user_name, password, 2],
+    [first_name, last_name, user_name, hashPass, 2],
     (err, result) => {
       if (!err) {
         res.status(201).json({ success: true, data: [] });
@@ -21,3 +22,9 @@ exports.register = asyncHandler(async (req, res, next) => {
     }
   );
 });
+
+encryptPass = async (password) => {
+  const salt = await bcrypt.genSaltSync(10);
+  const newPass = await bcrypt.hash(password, salt);
+  return newPass;
+};
